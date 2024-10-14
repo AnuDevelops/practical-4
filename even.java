d21227a02c2db6878c075e41223fc59e67413e79
AnuDevelops <158483171+AnuDevelops@users.noreply.github.com>
2024-10-14 14:49:20 +0100

    Update even.java

--- even.java
+++ even.java
@@ -1,1 +1,28 @@
+package practicals;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
+
+public class even {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList();
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the list of numbers: ");
+        while (scanner.hasNextInt()) {
+            int i = scanner.nextInt();
+            numbers.add(i);
+            if (i % 2 == 0) { 
+            System.out.println(i);
+        }
+        else {
+            System.out.println("These are the even numbers in your list.");
+        }
+        System.out.println(i);
+        
+        }
+    }
+}
